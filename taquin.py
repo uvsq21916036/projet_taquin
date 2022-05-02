5e278d5e7103457261cbb8d9a9aa847790337de9
uvsq21916036 <99216688+uvsq21916036@users.noreply.github.com>
2022-05-02 09:47:04 +0200

    Create taquin.py

---
+++ taquin.py
@@ -0,0 +1,9 @@
+#########################################
+# groupe 1 MIASHS TD2
+# '' projet taquin ''
+# Julien De Barros 
+# Sofian Amri
+# Leo Goncalves
+# Suleyman Charpentier
+#
+#########################################
